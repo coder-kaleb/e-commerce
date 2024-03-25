f564017e3e96435eed5068c5f19ee004c01e7c89
Kaleb Mikiyas <starbouns76@gmail.com>
2024-03-26 00:22:28 +0300

    simple-import-sort/imports fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Header } from './_components/Header'
 import { Providers } from './_providers'
 import { InitTheme } from './_providers/Theme/InitTheme'
 import { mergeOpenGraph } from './_utilities/mergeOpenGraph'
+
 import './_css/app.scss'
 
 const jost = Jost({
